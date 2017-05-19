07ea1c3a066f6eb2a0ffcbef5597c6ef9a26d63c
andro19951 <andro19951@gmail.com>
2017-05-20 02:27:09 +0300

    heh

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 var app = angular.module('minmax', []);
 
 app.controller('map', function ($scope,ContactService) {
-	
+
 console.log("andro");
 $scope.places=ContactService;
-console.log($scope.places[1]);
 });
 
 
@@ -13,8 +12,8 @@ app.service('ContactService', function () {
 	return {
 		"places": [
 		{
-			"lat":59.4370, 
-			"ling":24.7536,
+			"lat":59.4369, 
+			"ling":24.7533,
 			"wat":1
 		},
 		{
@@ -22,15 +21,15 @@ app.service('ContactService', function () {
 			"ling":24.7536,
 			"wat":1
 		},		{
-			"lat":59.4371, 
+			"lat":59.4352, 
 			"ling":24.7536,
 			"wat":1
 		},		{
-			"lat":59.4371, 
+			"lat":59.4373, 
 			"ling":24.7536,
 			"wat":1
 		},		{
-			"lat":59.437, 
+			"lat":59.4374, 
 			"ling":24.7536,
 			"wat":1
 		},		
